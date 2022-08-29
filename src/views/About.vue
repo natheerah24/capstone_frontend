ff05ea06632768ea024a59667dd8990e69b08972
Natheerah24 <natheerah24@gmail.com>
2022-08-29 16:27:32 +0200

    landing and about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,49 @@
-<template></template>
+<template>
+  <section id="about">
+    <div class="container text-center">
+      <h1 id="heading">About</h1>
+      <div class="row justify-content-center">
+        <div class="col-md-8" id="left">
+          <img
+            id="about-pic"
+            src="https://i.postimg.cc/9M7s4Gzf/pexels-karolina-grabowska-4016511-removebg-preview.png"
+            alt=""
+          />
+        </div>
+        <div class="col-md-4" id="right">
+          
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 <script>
-import aboutDesc from "../components/aboutDesc.vue";
-export default {
-  components: {
-    aboutDesc,
-  },
-};
+export default {};
 </script>
-<style></style>
+<style scoped>
+.row.justify-content-center {
+  display: flex;
+
+  justify-content: center;
+}
+#right {
+  background-color: #ab87ff;
+  min-width: 50vw;
+  min-height: 100vh;
+}
+#left {
+  background-color: #fface4;
+  min-width: 40vw;
+  min-height: 100vh;
+}
+#about-pic {
+  width: 100%;
+  height: 100%;
+}
+#heading {
+  font-family: "DynaPuff", cursive;
+  font-size: 6rem;
+  color: #ab87ffb6;
+  text-shadow: 2px 2px 5px #ff79d2;
+}
+</style>
